1b56d6febbd320ec7b4093c28583b16b47352d58
Akash ‎ <kingakash10@gmail.com>
2026-06-23 09:04:59 +0530

    gave it a professionla look

--- chatbot.py
+++ chatbot.py
@@ -8,14 +8,25 @@ client = Groq()
 messages = [
     {
         "role": "system",
-        "content": "You are a cool superhero with sarcasm. Give answers in short NOT long paragraphs"
+        "content": """
+        You are a cool superhero with sarcasm. 
+        Give answers in short 
+        NOT long paragraphs
+        """
     }
 ]
 
+print("=" * 50)
+print("AI Tutor Started")
+print("Type 'exit' to quit")
+print("=" * 50)
+
 while True:
-    user_input = input("User: ")
+
+    user_input = input("\nYou: ")
+
     if user_input.lower() == "exit":
-        print("AI: Goodbye!!")
+        print("\nAI: Goodbye!")
         break
 
     messages.append(
@@ -25,18 +36,23 @@ while True:
         }
     )
 
-    chat_completion = client.chat.completions.create(
-        messages = messages,
-        model = "llama-3.3-70b-versatile"
-    )
+    try:
 
-    response = chat_completion.choices[0].message.content
+        response = client.chat.completions.create(
+            messages=messages,
+            model="llama-3.3-70b-versatile"
+        )
 
-    print("AI:", response)
+        answer = response.choices[0].message.content
 
-    messages.append(
-        {
-            "role":"assitant",
-            "content":response
-        }
-    )
+        print(f"\nAI: {answer}")
+
+        messages.append(
+            {
+                "role": "assistant",
+                "content": answer
+            }
+        )
+
+    except Exception as e:
+        print(f"\nError: {e}")
